49aeb8b2f2840564968ddbe0981f46584307b7a9
Maged <eng.maged91@hotmail.com>
2013-10-05 22:56:41 +0200

    upload files

---
+++ QR app/QR app/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace QR_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.qrimage = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.url = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.qrimage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // qrimage
+            // 
+            this.qrimage.Location = new System.Drawing.Point(13, 13);
+            this.qrimage.Name = "qrimage";
+            this.qrimage.Size = new System.Drawing.Size(259, 210);
+            this.qrimage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.qrimage.TabIndex = 0;
+            this.qrimage.TabStop = false;
+            this.qrimage.Click += new System.EventHandler(this.qrimage_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Text";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // url
+            // 
+            this.url.Location = new System.Drawing.Point(38, 228);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(234, 20);
+            this.url.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(259, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save QR code";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 311);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(260, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Extract QR code Data";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 337);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.url);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.qrimage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Gazzar QR Code";
+            ((System.ComponentModel.ISupportInitialize)(this.qrimage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox qrimage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ QR app/QR app/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// here add QR library
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+
+namespace QR_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Save button
+        /// </summary>
+      
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog(); //Prompts the user to select a location for saving a file.
+            s.Filter = "PNG|*.png|JPEG|*.jpeg|BMP|*.bmp|GIF|*.gif";
+            if(s.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                qrimage.Image.Save(s.FileName);
+            }
+        }
+
+
+        /// <summary>
+        /// create buuton : create QR code
+        /// </summary>
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string URL = url.Text; // textbox string
+            QRCodeEncoder encoder = new QRCodeEncoder();
+            Bitmap qrcode = encoder.Encode(URL); // encode url into the image
+            qrimage.Image = qrcode as Image; // present image in the imagebox
+        }
+
+        /// <summary>
+        /// extract code from image
+        /// </summary>
+     
+        private void button4_Click(object sender, EventArgs e)
+        {
+            QRCodeDecoder decoder = new QRCodeDecoder();
+            MessageBox.Show(decoder.decode(new QRCodeBitmapImage(qrimage.Image as Bitmap)));
+        }
+
+        private void qrimage_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
